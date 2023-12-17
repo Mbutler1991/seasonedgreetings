01550f8b0f54b70093890e98aebc6bee8ae1ac79
Kera Cudmore <kera.cudmore@hotmail.com>
2023-12-17 14:35:26 +0000

    refactor: fix code indentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,45 @@
 // Contact Page Script
-    document.addEventListener('DOMContentLoaded', function () {
-      const form = document.querySelector('.contact-form');
-      const phoneInput = document.getElementById('phone');
-      let errorMessageShown = false;
+document.addEventListener('DOMContentLoaded', function () {
 
-      form.addEventListener('submit', function (event) {
-        const phoneValue = phoneInput.value.trim();
+  const form = document.querySelector('.contact-form');
+  const phoneInput = document.getElementById('phone');
+  let errorMessageShown = false;
 
-        // Define the regular expression for a valid international phone number
-        const phoneRegex = /^\+\d{1,3}\s?\d{1,}$/;
+  form.addEventListener('submit', function (event) {
+    const phoneValue = phoneInput.value.trim();
 
-        if (!phoneRegex.test(phoneValue)) {
-          if (!errorMessageShown) {
-            // Create a new error message element
-            const errorMessage = document.createElement('div');
-            errorMessage.classList.add('error-message');
-            errorMessage.textContent = 'Please enter a valid international phone number with the country code. For example, +123 4567890';
+    // Define the regular expression for a valid international phone number
+    const phoneRegex = /^\+\d{1,3}\s?\d{1,}$/;
 
-            // Insert the error message after the phone input
-            phoneInput.parentNode.insertBefore(errorMessage, phoneInput.nextSibling);
+    if (!phoneRegex.test(phoneValue)) {
+      if (!errorMessageShown) {
 
-            // Highlight the input field to draw attention
-            phoneInput.classList.add('error');
+        // Create a new error message element
+        const errorMessage = document.createElement('div');
+        errorMessage.classList.add('error-message');
+        errorMessage.textContent = 'Please enter a valid international phone number with the country code. For example, +123 4567890';
 
-            errorMessageShown = true; // Set the flag to true
-          }
+        // Insert the error message after the phone input
+        phoneInput.parentNode.insertBefore(errorMessage, phoneInput.nextSibling);
 
-          // Prevent form submission
-          event.preventDefault();
-        }
-      });
+        // Highlight the input field to draw attention
+        phoneInput.classList.add('error');
 
-      // Remove the error message and styling when the phone input is focused
-      phoneInput.addEventListener('focus', function () {
-        const errorMessage = document.querySelector('.error-message');
-        if (errorMessage) {
-          errorMessage.remove();
-          phoneInput.classList.remove('error');
-          errorMessageShown = false; // Reset the flag
-        }
-      });
-    });
+        errorMessageShown = true; // Set the flag to true
+      }
+
+      // Prevent form submission
+      event.preventDefault();
+    }
+  });
+
+  // Remove the error message and styling when the phone input is focused
+  phoneInput.addEventListener('focus', function () {
+    const errorMessage = document.querySelector('.error-message');
+    if (errorMessage) {
+      errorMessage.remove();
+      phoneInput.classList.remove('error');
+      errorMessageShown = false; // Reset the flag
+    }
+  });
+});
